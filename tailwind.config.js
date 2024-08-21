c77cbbd9e8d43085ff998c02c0b4aad87435f13d
Tiswan <tiswan30@gmail.com>
2024-08-21 23:29:20 +0700

    first commit by wann

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ['./index.html', './src/**/*.js'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#EAE9E6',
+        secondary: '#1B1B1B',
+        triatry: '#E74522',
+        lightf: '#FFFFFF',
+        nav: '#1B1B1B',
+      },
+      fontFamily: {
+        delta: ['Dela Gothic One', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
