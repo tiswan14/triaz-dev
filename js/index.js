e5a57fae4754ce3730b3327e44dc3bd9a3b26b57
Muhammad Randika <dandimp2001@gmail.com>
2024-08-30 23:45:02 +0700

    Tingal prototype tombol link dll

--- js/index.js
+++ js/index.js
@@ -3,9 +3,9 @@ window.onscroll = () => {
     const fixednav = header.offsetTop;
 
 
-    if(window.pageYOffset > fixednav) {
+    if (window.pageYOffset > fixednav) {
         header.classList.add('navbar-fixed');
-    }else {
+    } else {
         header.classList.remove('navbar-fixed');
     }
 };
@@ -16,4 +16,36 @@ const navMenu = document.querySelector('#navMenu')
 navbar.addEventListener('click', () => {
     navbar.classList.toggle('hamburger-active');
     navMenu.classList.toggle('hidden');
+})
+
+// Untuk Homepage feedback
+const button = document.getElementById('button');
+const nama = document.getElementById('namaAnda').value;
+const email = document.getElementById('emailAnda').value;
+const pesan = document.getElementById('pesanAnda').value;
+
+
+button.addEventListener('click', () => {
+    if (nama.value && email.value && pesan.value) {
+        alert('Pesan terkirim');
+
+        document.getElementById('namaAnda').value = '';
+        document.getElementById('emailAnda').value = '';
+        document.getElementById('pesanAnda').value = '';
+    } else {
+        alert('Tolong masukkan semua informasi: nama, email, dan pesan');
+
+        document.getElementById('namaAnda').value = '';
+        document.getElementById('emailAnda').value = '';
+        document.getElementById('pesanAnda').value = '';
+    }
+});
+// Akhir Untuk Homepage Pesan
+
+// Hukum dan management
+
+const hukum = document.getElementById('hukum');
+hukum.addEventListener('click', () => {
+    event.preventDefault();
+    alert("Belum Tersedia Cooming Soonn !!!");
 })
